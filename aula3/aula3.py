9c38709b75dcb9f5108418b7186876124702b97d
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-08-23 15:23:11 -0300

    fim da aula 3

---
+++ aula3/aula3.py
@@ -0,0 +1,7 @@
+print('Essa é uma "string" (str)')
+print("Essa é uma 'string' (str)")
+print('Esse é o meu texto \'texto\'')
+print("Esse é o meu texto \"texto\"")
+print("Quero colocar um barra n dentro da str \\n")
+print(r'\n')
+

d850b1596c417b2bbd98ad2cffc80bc815413546
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-08-23 15:52:45 -0300

    fim da aula 4

---
+++ aula4/aula4.py
@@ -0,0 +1,14 @@
+print(type('Raphael'))
+print(type(123))
+print(type(123.7))
+print(type(True))
+print(bool(''))
+
+# type casting
+print(type(str(123)))
+
+print('Meu nome é Raphael Barros', type('Raphael'))
+print('Minha idade:', 38, type(38))
+print('Minha peso:', 83.5, type(83.5))
+print('Sou maior de idade:', 38 >= 18, type(38 >=18))
+

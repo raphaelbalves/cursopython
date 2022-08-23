d752ec58da15a308a8a2d2f34ca26a4beceeb301
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-08-23 10:46:19 -0300

    mudança na aula 1

---
+++ aula1/aula1.py
@@ -0,0 +1,9 @@
+print('linha1')
+print('linha2')
+# Estou aqui fazendo um comentário para entender melhor o código depois.
+# O Python lê o código da esquerda para a direita e de cima para baixo.
+# Quando encontra o comentário, ele desconsidera.
+print('linha3')
+print('linha4')
+print('linha5')
+print('linha6')

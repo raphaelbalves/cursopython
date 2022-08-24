b60003b38b7fc2bb86cfedf27e82b68154c4b851
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-08-24 14:45:53 -0300

    fim da aula 12

---
+++ aula12/aula12.py
@@ -0,0 +1,12 @@
+a = ''
+b = 0
+
+if not a:
+    print('Preencha o valor de "a"')
+
+
+if 'a' in 'raphael':
+    print('A letra "a" está na palavra "Raphael"')
+
+if 'w' not in 'raphael':
+    print('A letra "w" não está na palavra "Raphael"')

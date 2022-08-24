aa8cc9f15678c3571ec7b96ca8c0a5a2d5612fe4
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-08-23 22:10:24 -0300

    fim da aula 6

---
+++ aula6/aula6.py
@@ -0,0 +1,13 @@
+nome = 'Raphael'
+idade = 38
+altura = 1.74
+e_maior = idade > 18
+data_1 = True
+data_atual = 2022
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('Maior de idade:', e_maior)
+print('Tem data?', data_1)
+print('Ano de vigência:', data_atual)

d4138992dd771c215fec7a1821695f245ac7071a
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-08-24 10:03:12 -0300

    últimas atualizações

---
+++ aula7/aula7.py
@@ -0,0 +1,4 @@
+nome = 'Raphael'
+idade = 38.343498393284
+print(f'Meu nome é {nome} e minha idade é {idade:.2f} anos')
+print('{1} é meu nome e tenho {0} anos'.format(nome, idade))
---
+++ aula8/aula8.py
@@ -0,0 +1,11 @@
+nome = 'Raphael Barros'
+idade = 38
+ano_atual = 2022
+ano_nascimento = 2022 - 38
+peso = 84
+altura = 1.74
+imc = peso / altura ** 2
+
+print(f'Meu nome é {nome}, tenho {idade} anos e estamos no ano de {ano_atual}.'
+      f' Peso {peso} kg, tenho {altura} m de altura e meu IMC é {imc:.2f} -- perceberam que só tem duas casas decimais?'
+      f' Ia esquecendo. Nasci em {ano_nascimento}.')

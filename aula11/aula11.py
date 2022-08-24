3db1426315b3dc8a06b6c6a9463b21c4b093aec4
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-08-24 12:36:09 -0300

    fim aula 11

--- aula11/aula11.py
+++ aula11/aula11.py
@@ -3,9 +3,9 @@ print(2 == 2)
 
 nome = input('Qual o seu nome: ')
 idade = int(input('Qual a sua idade: '))
-idade_limite = 18
 
-if idade >= idade_limite:
+
+if idade >= 20 and idade <= 30:
     print(f'{nome} pode pegar o empréstimo.')
 else:
     print(f'{nome} não pode pegar o empréstimo.')

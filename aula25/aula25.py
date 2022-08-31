72ab50c71abc5fc34a587780f3c1ed7c0b4e041f
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-08-31 20:24:11 -0300

    fim da aula 25

--- aula25/aula25.py
+++ aula25/aula25.py
@@ -1,18 +1,49 @@
-frase = 'O Brasil é o país do futebol, o Brasil é penta.'
-dividido = frase.lower().split()
-lista = []
+# frase = 'O Brasil é o país do futebol, o Brasil é penta.'
+# dividido = frase.lower().split()
+# lista = []
+#
+# palavra = ''
+# contagem = 0
+#
+# for x in dividido:
+#     maior = dividido.count(x)
+#     if maior > contagem:
+#         contagem = maior
+#         palavra = x
+#
+#     if x not in lista:
+#         lista.append(x.)
+#         print(f'"{x}" aparece {dividido.count(x)}x na frase.')
+#
+# print(f'A palavra que mais apareceu no texto foi "{palavra}": {contagem} vezes.')
 
-palavra = ''
-contagem = 0
+# frase = 'O Brasil é penta.'
+# lista = frase.split()
+# frase2 = ' '.join(lista)
+# print(frase2)
 
-for x in dividido:
-    maior = dividido.count(x)
-    if maior > contagem:
-        contagem = maior
-        palavra = x
+# frase = 'O Brasil é penta.'
+# lista = frase.split()
+#
+# for v1, v2 in enumerate(lista):
+#     print(v1,v2)
 
-    if x not in lista:
-        lista.append(x.)
-        print(f'"{x}" aparece {dividido.count(x)}x na frase.')
+lista = [
+    [0, 'Rphl'],
+    [1, 'Iana'],
+    [2, 'Juca']
+]
 
-print(f'A palavra que mais apareceu no texto foi "{palavra}": {contagem} vezes.')
+# for indice, nome in lista:
+#     print(indice, nome)
+#
+# for x, y in enumerate(lista):
+#     print(x, y)
+
+# for x in lista:
+#     for y in x:
+#         if y == lista[-1][1]:
+#             print(y)
+#         else:
+#             print(y, end=',')
+

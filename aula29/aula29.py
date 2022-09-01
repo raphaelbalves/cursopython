3d2179e2bbbe0622dac20fc36e122763df8ed195
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-09-01 15:41:40 -0300

    fim da aula 29

---
+++ aula29/aula29.py
@@ -0,0 +1,17 @@
+logged_user = False
+
+# if logged_user:
+#     msg = 'Usuário logado.'
+# else:
+#     msg = 'Usuário precisa logar.'
+#
+# print(msg)
+
+# msg='Usuário logado.' if logged_user else 'Usuário precisa logar.'
+#
+# print(msg)
+
+idade = 17
+
+entrada = 'pode entrar' if idade >= 18 else 'fique de fora'
+print(entrada)

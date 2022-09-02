35cf1d3af42cdf193c34e06d9aeb8dcc6c4b525f
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-09-02 12:54:43 -0300

    aula 30, 31, e 32

---
+++ aula30/aula30.py
@@ -0,0 +1,22 @@
+# nome = input('Qual o seu nome: ')
+
+# if nome:
+#     print(nome)
+# else:
+#     print('vc nao digitou nada')
+
+# print(nome or 'voce nao digitou nada')
+
+# print(nome) if nome else print('vc nao digitou nada')
+
+a = 0
+b = None
+c = False
+d = []
+e = {}
+f = 22
+g = 'luiz'
+
+variavel = a or b or c or d or e or f or g
+
+print(variavel)
---
+++ aula31/aula31.py
@@ -0,0 +1,12 @@
+# for x, y in enumerate(range(10, 1, -1)):
+#     print(x, y)
+#
+#
+
+asc = 0
+desc = 10
+
+while asc <= 8:
+    print(asc, desc)
+    asc += 1
+    desc -= 1

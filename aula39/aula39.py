0a7ebd0bc35ad8d0c5f9a5457bf04b04b56d030c
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-09-09 13:27:26 -0300

    fim da aula 39

--- aula39/aula39.py
+++ aula39/aula39.py
@@ -1,29 +1,26 @@
-# Exercício 01
+# # Exercício 01
+#
+# def func1(arg):
+#     return arg()
+#
+# def func2():
+#     return 'Olá'
+#
+# print(func1(func2))
 
-def func1(arg):
-    return arg
-
-def func2():
-    return 'Olá'
-
-print(func1(func2()))
 
 # Exercício 02
 
-def func1(*arg):
-    return arg
+def mestre(funcao, *args, **kwargs):
+    return funcao(*args, **kwargs)
 
-def func2(*args):
-    return args
+def fala_oi(nome=''):
+    return f'Oi, {nome}'
 
-print(func1(func2('cabeça', 'de'), func2('dinossauro')))
-
-def fala_oi(nome):
-    return f'Oi {nome}'
-
-def saudacao(saudacao, nome):
+def saudacao(nome, saudacao):
     return f'{saudacao} {nome}'
 
-print(func1(fala_oi('carlos'), saudacao('hey', 'romero')))
-
-
+executando = mestre(fala_oi, 'Luiz')
+executando2 = mestre(saudacao, saudacao='ola', nome='luiz')
+print(executando)
+print(executando2)

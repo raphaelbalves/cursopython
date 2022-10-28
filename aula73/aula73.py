570520ec6d10222ca7eb0cb258ff8119fbb198cf
Raphael Barros <raphaelbarrosalves@gmail.com>
2022-10-28 15:56:37 -0300

    fim da aula 73

---
+++ aula73/aula73.py
@@ -0,0 +1,18 @@
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        print(self.ano_atual - self.idade)
+
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimento):
+        idade = cls.ano_atual - ano_nascimento
+        return cls(nome, idade)
+
+p1 = Pessoa('Luiz', 32)
+p2 = Pessoa.por_ano_nascimento('rphl', 1984)
+print(p2.nome, p2.idade)
